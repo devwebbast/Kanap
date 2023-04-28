998650bbdaae854fbc2de4408f288aba400f29cb
DevBastFront <devbastfront@gmail.com>
2023-04-28 20:37:33 +0200

    V1.1.2

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,21 +1,27 @@
 // produit enregistré dns le localstorage
-let productToLocalStorage = JSON.parse(localStorage.getItem("productCart")) // converti les donnés au format JSON dans le localstorage en oject JS
+let products = JSON.parse(localStorage.getItem("productCart")) // converti les donnés au format JSON dans le localstorage en object JS
 
 
-// Affichage du contenu du panier
-  let cartList = ''
-  for (let k = 0; k < productToLocalStorage.length; k++){
-  cartList = cartList + `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+async function main(){
+      let cartHtmlList = ""
+      let totalPrice = 0
+       for (let k = 0; k < products.length; k++){
+        const product = products[k] 
+        totalPrice = totalPrice + product.price * product.quantity
+        cartHtmlList = cartHtmlList + `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+            <div class="cart__item__img">
+              <img src="${product.imageUrl}" alt="Photographie d'un canapé">
+            </div>
             <div class="cart__item__content">
               <div class="cart__item__content__description">
-                <h2>${productToLocalStorage.name}</h2>
-                <p>Vert</p>
-                <p>${productToLocalStorage.price} €</p>
+                <h2>${product.name}</h2>
+                <p>${product.color}</p>
+                <p>${product.price} €</p>
               </div>
               <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
                   <p>Qté : </p>
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
                 </div>
                 <div class="cart__item__content__settings__delete">
                   <p class="deleteItem">Supprimer</p>
@@ -23,9 +29,38 @@ let productToLocalStorage = JSON.parse(localStorage.getItem("productCart")) // c
               </div>
             </div>
           </article>`
-  document.querySelector("#cart__items").innerHTML = cartList
-  }
+          document.querySelector("#cart__items").innerHTML = cartHtmlList
+          document.querySelector("#totalPrice").innerHTML = totalPrice
+          
+        }
 
+        //modification de la quantité d'un produit du panier
+        let btnChangeQuantity = document.querySelectorAll(".itemQuantity")
+        for (let j = 0; j < btnChangeQuantity.length; j++){
+          btnChangeQuantity[j].addEventListener("change", function(){
+            products[j].quantity = btnChangeQuantity[j].value
+            localStorage.setItem("productCart", JSON.stringify(products))
+            alert('La quantité a bien été modifiée')
+            location.reload()
+          })
+        }
+
+
+        //suppression d'un produit du panier        
+        let btnDelete = document.querySelectorAll(".deleteItem")
+        for (let l = 0; l < btnDelete.length; l++){
+          btnDelete[l].addEventListener("click", function(){
+            products.splice(l, 1)
+            localStorage.setItem("productCart", JSON.stringify(products))
+            alert('Le produit a bien été supprimé du panier')
+            location.reload()
+          })
+        }
+            
+
+
+}
+main()
 
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -2,12 +2,14 @@
 const urlParams = new URLSearchParams(document.location.search)
 const id = urlParams.get('id')
 const UrlApi = `http://localhost:3000/api/products/${id}`
+let selectedProduct = {}
 
 // Récupération des information du produit
 fetch(UrlApi)
   .then(response => response.json())
   .then((product) => {
 
+    selectedProduct = product
     // Ajout de l'image
     let productImage = document.createElement('img')
     productImage.setAttribute('src', product.imageUrl)
@@ -40,31 +42,66 @@ fetch(UrlApi)
       let colorChoice = document.querySelector("#colors").value
       let productCart = {
         id: id,
+        imageUrl : selectedProduct.imageUrl,
+        name: selectedProduct.name,
+        price: selectedProduct.price,
         quantity: quantityChoice,
         color: colorChoice
       }
       if (productCart.color == "" && productCart.quantity <= 0) {
         alert("Veuillez choisir une couleur et une quantité")
-      }else if (productCart.color == "") {
+        return -1
+        
+      }
+      if (productCart.color == "") {
         alert("Veuillez choisir une couleur")
-      }else if (productCart.quantity <= 0) {
+        return -1
+      }
+      if (productCart.quantity <= 0) {
         alert("Veuillez choisir une quantité") 
-      }   
+        return -1
+      }
+      
+      const productAdded = () => {
+        if(window.confirm(`Votre produit a bien été ajouté au panier 
+Cliquez sur ok pour continuer`)){
+          window.location.href = "cart.html"
+        }
+      }
+      
       //produit enregistré dans le local storage
-      let productToLocalStorage = JSON.parse(localStorage.getItem("productCart")) // converti les donnés au format JSON dans le localstorage en oject JS
-      console.log(productToLocalStorage)
-
-
+      let productToLocalStorage = JSON.parse(localStorage.getItem("productCart")) // converti les donnés au format JSON dans le localstorage en object JS
+      
+      
       // si il y a déjà des produit dans le local storage
       if(productToLocalStorage){
+        // Vérification si un produit est déjà présent dans le local storage
+        const productInCart = productToLocalStorage.find(product => product.id === id && product.color === colorChoice)
+
+        // nouveau produit ajouté dans le local storage
+        if(productInCart){
+          let newProductAdded = parseInt(productInCart.quantity) + parseInt(quantityChoice)
+          productInCart.quantity = newProductAdded
+          localStorage.setItem("productCart", JSON.stringify(productToLocalStorage))
+          productAdded()
+
+        // produit déjà présent, on utilise .push() pour ajouter le nouveau produit dans le local storage
+        }else{
         productToLocalStorage.push(productCart)
         localStorage.setItem("productCart", JSON.stringify(productToLocalStorage))
-      }else{
+        productAdded()
+        }
+
+      // si il n'y a pas de produit dans le local storage  
+        }else{
         productToLocalStorage = []
         productToLocalStorage.push(productCart)
         localStorage.setItem("productCart", JSON.stringify(productToLocalStorage))
-      }
-
+        productAdded()
+        }
     })
-  }
+}
+    
+
+    
   addToCart ()  
